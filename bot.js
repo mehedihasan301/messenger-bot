b377b1cebff5d64c3b589255f879e132fc2d8a75
mehedihasan301 <cm.mehedi99@gmail.com>
2025-05-08 23:31:22 +0600

    bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,21 @@
-bot.js
+const login = require("facebook-chat-api");
+
+login({ email: process.env.EMAIL, password: process.env.PASSWORD }, (err, api) => {
+  if (err) return console.error("Login error:", err);
+
+  console.log("Bot is now listening...");
+
+  api.listenMqtt((err, message) => {
+    if (err) return console.error(err);
+
+    const command = message.body.toLowerCase();
+
+    if (command === "!hello") {
+      api.sendMessage("Hello from your bot!", message.threadID);
+    }
+
+    if (command === "!anime") {
+      api.sendMessage("Try One Piece, Jujutsu Kaisen or Demon Slayer!", message.threadID);
+    }
+  });
+});
